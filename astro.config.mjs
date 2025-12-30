a8ac9e88934d144e67f719d3bceb564d41b95bb4
Ridwan Maulana <rmfhdofficial@gmail.com>
2025-12-30 12:24:46 +0700

    opo wae kuy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react'; // Jika tadi install react juga
 
 export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
+  
   integrations: [tailwind(), react()]
 });
